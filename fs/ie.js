6c0084146c08e2ed73c8eb8366b9447e82409c38
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-11-16 12:09:46 +0100

    removed unnecessary files

--- fs/ie.js
+++
@@ -1,4 +0,0 @@
-document.write('<script src="https://unpkg.com/@ungap/custom-event"></script>');
-document.write('<script src="https://cdn.jsdelivr.net/npm/promise-polyfill@8/dist/polyfill.min.js"></script>');
-
-document.documentElement.className += ' ie';
